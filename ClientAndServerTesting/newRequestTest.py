49f835db2694e7ea2e480393758c690d46650acd
Ivan <ilian001@ucr.edu>
2020-05-28 11:27:47 -0700

    different variables added

--- ClientAndServerTesting/newRequestTest.py
+++ ClientAndServerTesting/newRequestTest.py
@@ -7,8 +7,8 @@ from RequestTest import serverAndClientTest
 
 def main():
 	serverAndClientTest(0.12,0.80,0.10,"open_hand")
-	serverAndClientTest(0.12,0.80,0.10,"open_hand")	
-	serverAndClientTest(0.12,0.80,0.10,"open_hand")
+	serverAndClientTest(0.13,0.81,0.11,"open_hand")	
+	serverAndClientTest(0.14,0.82,0.12,"open_hand")
 
 if __name__ == "__main__":
 	try:
